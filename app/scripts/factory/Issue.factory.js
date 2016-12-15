9e86ccc535bb210f8189012751f465c8bf1455ea
alina52 <alinawang1125@gmail.com>
2016-12-15 21:29:43 +0800

    可查看自己的任务

--- app/scripts/factory/Issue.factory.js
+++ app/scripts/factory/Issue.factory.js
@@ -6,7 +6,7 @@
 angular.module('crowdsourcing')
   .factory('IssueFactory', function($resource, $rootScope, SessionService) {
     var baseUrl = base_Url;
-    var XbaseUrl = xie_base_Url+'/issues';
+    var XbaseUrl = xie_base_Url+'/issues/mine';
     return {
 
       getIssueList: function() {
