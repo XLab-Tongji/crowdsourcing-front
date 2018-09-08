1572bc2ff819147396e66077afbdd2ad61af93b9
niyw <nee_11235@qq.com>
2018-09-08 15:50:55 +0800

    remove dist/

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 'use strict';
-module.exports = function (grunt) {
 
+module.exports = function (grunt) {
     // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
 
@@ -15,7 +15,6 @@ module.exports = function (grunt) {
 
     // Grunt configuration
     grunt.initConfig({
-
         // Project settings
         crowdsourcing: appConfig,
 
