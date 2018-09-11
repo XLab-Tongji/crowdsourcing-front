77eda86620eba725c91b6d71ab418eb4377f4158
jaki2012 <jaki2012@sina.cn>
2018-09-12 04:53:24 +0800

    measures.html 分页布局测试和调试完成

--- dist/scripts/filter/filter.e8452298.js
+++
@@ -1,7 +0,0 @@
-angular.module('crowdsourcing').
-  filter('htmlToPlaintext', function() {
-    return function(text) {
-      return  text ? String(text).replace(/<[^>]+>/gm, '') : '';
-    };
-  }
-);
